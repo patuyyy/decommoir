7d4d155c26ec22e0f13a3d1c54da128b5f04c159
patuyyy <han.ihsan01@gmail.com>
2026-01-21 15:22:08 +0700

    cicd: add loadbalancer.tf for lb provisioning

---
+++ terraform/loadbalancer.tf
@@ -0,0 +1,45 @@
+resource "google_compute_backend_service" "frontend_backend" {
+  name                  = "frontend-backend"
+  protocol              = "HTTP"
+  port_name             = "http"
+  load_balancing_scheme = "EXTERNAL"
+  timeout_sec           = 30
+
+  health_checks = [
+    google_compute_health_check.frontend_hc.id
+  ]
+
+  backend {
+    group = google_compute_instance_group_manager.frontend_mig.instance_group
+  }
+}
+
+resource "google_compute_url_map" "web_map" {
+  name            = "decommoir-url-map"
+  default_service = google_compute_backend_service.frontend_backend.id
+}
+
+resource "google_compute_target_http_proxy" "http_proxy" {
+  name    = "decommoir-http-proxy"
+  url_map = google_compute_url_map.web_map.id
+}
+
+resource "google_compute_global_forwarding_rule" "http_forwarding" {
+  name                  = "decommoir-http-forwarding"
+  target                = google_compute_target_http_proxy.http_proxy.id
+  port_range            = "80"
+  load_balancing_scheme = "EXTERNAL"
+}
+
+resource "google_compute_firewall" "allow_lb_to_frontend" {
+  name    = "allow-lb-to-frontend"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
+  target_tags   = ["frontend"]
+}
